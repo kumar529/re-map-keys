4cb5748b2784dccd1bcf42472a836b7a265657a2
Rahul Kumar <rahul.iitr529@gmail.com>
2019-01-10 15:07:35 +0530

    Added test cases for string null or blank check

---
+++ test/lib/utils/UtilsTest.spec.ts
@@ -0,0 +1,25 @@
+import 'mocha';
+import { expect } from 'chai';
+import { Utils } from '../../../src/lib/utils/Utils';
+
+describe("null or blank string", () => {
+    it("is null or blank string", () => {
+        let input: string = "";
+        expect(Utils.isNullOrBlank(input)).to.equal(true);
+    })
+
+    it("is not null or blank string", () => {
+        let input: string = "test";
+        expect(Utils.isNullOrBlank(input)).to.equal(false);
+    })
+
+    it("is null or blank string array", () => {
+        let input: string[] = ["", ""];
+        expect(Utils.isNullOrBlankAny(input)).to.equal(true);
+    })
+
+    it("is not null or blank string array", () => {
+        let input: string[] = ["test1", "test2"];
+        expect(Utils.isNullOrBlankAny(input)).to.equal(false);
+    })
+});
